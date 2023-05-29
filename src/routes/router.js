5e086e5d336f796e6a431e6236330d51dbe5703f
CardinalRPH <rayhanfebriyan62@gmail.com>
2023-05-30 01:17:45 +0700

    add Route function guard

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,4 +1,6 @@
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
+
+import PrivateRoute from "../utils/PrivateRoute";
 
 import LnSuLayout from "../pages/layout/LnSuLayout";
 import Forget_Layout from "../pages/layout/ForgetPwLayout";
@@ -13,64 +15,69 @@ import Calendar_pg from "../pages/Calendar";
 import Cuaca_pg from "../pages/Cuaca";
 import Tips_pg from "../pages/Tips";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Navigate to="/login" replace />
-    },
+const AppRouter = createBrowserRouter([
+    // {
+    //     path: "/",
+    //     element: <Navigate to="/login" replace />
+    // },
     // {
     //     path: "/home",
     //     element: <LandingPage />
     // },
     {
         path: '/login',
-        element: <LnSuLayout>
-            <Login_pg />
-        </LnSuLayout>
+        element: (
+            <LnSuLayout>
+                <Login_pg />
+            </LnSuLayout>
+        )
     },
     {
         path: '/sign-up',
-        element: <LnSuLayout>
+        element: (<LnSuLayout>
             <Signup_pg />
-        </LnSuLayout>
+        </LnSuLayout>)
     },
     {
         path: '/forget-password',
-        element: <Forget_Layout>
-            <ForgetPw_pg />
-        </Forget_Layout>
+        element: (
+            <Forget_Layout>
+                <ForgetPw_pg />
+            </Forget_Layout>)
     },
     {
         path: '/forget-send',
-        element: <Forget_Layout>
-            <ForgetPwSend_pg />
-        </Forget_Layout>
+        element: (
+            <PrivateRoute>
+                <Forget_Layout>
+                    <ForgetPwSend_pg />
+                </Forget_Layout>
+            </PrivateRoute>)
     },
     {
         path: '/dashboard',
-        element: <Dash_Layout>
-            <Dashboard_pg />
-        </Dash_Layout>
+        // element: (<PrivateRoute component={Dash_Layout}>
+        //     <Dashboard_pg />
+        // </PrivateRoute>)
     },
     {
         path: '/calendar',
-        element: <Dash_Layout>
+        element: (<Dash_Layout>
             <Calendar_pg />
-        </Dash_Layout>
+        </Dash_Layout>)
     },
     {
         path: '/cuaca',
-        element: <Dash_Layout>
+        element: (<Dash_Layout>
             <Cuaca_pg />
-        </Dash_Layout>
+        </Dash_Layout>)
     },
     {
         path: '/tips',
-        element: <Dash_Layout>
+        element: (<Dash_Layout>
             <Tips_pg />
-        </Dash_Layout>
+        </Dash_Layout>)
     },
 ]);
 
-
-export default router;
+export default AppRouter;
