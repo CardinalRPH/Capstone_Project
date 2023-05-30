0b7d9a1665073ccd6fd6230ef67923a9e8c9536b
CardinalRPH <rayhanfebriyan62@gmail.com>
2023-05-31 00:28:43 +0700

    Fixing CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path'
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
 
 import { default as indexRouter } from './routes/index.js';
 import { default as usersRouter } from './routes/users.js';
@@ -17,5 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(cors({
+    origin: 'http://localhost',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 export default app;
