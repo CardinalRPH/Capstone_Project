e5948c7600625ac20d334a333ddd706b537d8a96
CardinalRPH <rayhanfebriyan62@gmail.com>
2023-06-15 15:34:20 +0700

    Add port

--- libs/db.config.js
+++ libs/db.config.js
@@ -9,7 +9,8 @@ const sequelizes = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIAL
+        dialect: process.env.DB_DIAL,
+        port: process.env.DB_PORT
     }
 );
 
